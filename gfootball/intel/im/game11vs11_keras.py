f025402e92ee095f0231d4e664b7b50288b8b22a
songhappy <guoqiongsong@gmail.com>
2020-10-20 16:03:42 -0700

    zoo model imitation learning

--- gfootball/intel/im/game11vs11_keras.py
+++ gfootball/intel/im/game11vs11_keras.py
@@ -1,10 +1,9 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-from gfootball.intel.im.models import MovementPredictor
+from gfootball.intel.im.models import MovementPredictorKeras
 from gfootball.intel.im.preprocess import *
 from matplotlib import pyplot as plt
-
 import time
 
 TRAIN = True
@@ -12,11 +11,11 @@ RENDER = False
 DUMP = False
 Score_length = 50
 filter_positive = False
-model_path = "/home/arda/intelWork/projects/googleFootball/11vs11/dumpFeb18_model_2"
+model_path = "/home/arda/intelWork/projects/googleFootball/dumpSep22_model_keras"
 
 
 def main():
-    input_path = "/home/arda/intelWork/projects/googleFootball/11vs11/dumpMar16_score/"
+    input_path = "/home/arda/intelWork/projects/googleFootball/dumpSep22_score/"
 
     players = ['agent:left_players=1']
     cfg = config.Config({
@@ -30,18 +29,19 @@ def main():
 
     actions_size = len(action_set_dict["default"])
     feature_size = 195
-    if (not TRAIN):
+    if (TRAIN):
         labels, states = load_data(input_path,"score", score_length, filter_positive)
         print(labels.shape)
         print(states.shape)
-        train_split = 1400000
-        val_split=10000
+        #sys.exit()
+        train_split = 1360000
+        val_split=20000
         train_labels = labels[0:train_split]; val_labels = labels[-val_split:-1]
         train_states= states[0:train_split]; val_states = states[-val_split:-1]
 
-        agent = MovementPredictor(actions_size, [feature_size])
+        agent = MovementPredictorKeras(actions_size, [feature_size])
         begin = time.time()
-        nepochs = 20
+        nepochs = 1
         history = agent.train(train_states, train_labels, 64, epoch=nepochs, model_path=model_path)
         loss = history.history["loss"]
         accuracy = history.history["accuracy"]
@@ -49,7 +49,6 @@ def main():
         val_accruray = history.history["val_accuracy"]
         for i in range(nepochs):
             print(loss[i], val_loss[i], accuracy[i], val_accruray[i])
-
 
         plt.plot(loss,"b")
         plt.plot(val_loss,"g")
@@ -66,7 +65,7 @@ def main():
         if RENDER:
             env.render()
         env.reset()
-        agent = MovementPredictor(actions_size, [feature_size])
+        agent = MovementPredictorKeras(actions_size, [feature_size])
         agent.load(model_path +"/best_model.h5")
         #print(agent.model.get_weights())
         obs, reward, done, info = env.step(env.action_space.sample())
@@ -79,7 +78,7 @@ def main():
             action = agent.act(feature)
             #action = env.action_space.sample()
             obs, reward, done, info = env.step(action)
-            print("reward:", reward)
+            #print("reward:", reward)
             episode_reward = episode_reward + reward
             if done:
                 env.reset()
@@ -90,6 +89,5 @@ def main():
                 print("episode:{}".format(nepisode), "score:{}".format(obs[0]["score"]))
                 episode_reward = 0
         print("win:{}".format(win), "lose:{}".format(lose))
-
 if __name__ == '__main__':
     main()
